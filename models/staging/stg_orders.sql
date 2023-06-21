3ca9f17cfe8282861697d1f293e1d17e41b7a71f
Sebastian Wiesner <sebastian_22@icloud.com>
2023-06-21 14:07:27 +0000

    clean up, running demo project with snowflake as database

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,33 +1,6 @@
-with
-
-source as (
-
-    select * from {{ source('ecom', 'raw_orders') }}
-
-    -- data runs to 2026, truncate timespan to desired range,
-    -- current time as default
-    where ordered_at <= {{ var('truncate_timespan_to') }}
-
-),
-
-renamed as (
-
-    select
-
-        ----------  ids
-        id as order_id,
-        store_id as location_id,
-        customer as customer_id,
-
-        ---------- properties
-        (order_total / 100.0)::float as order_total,
-        (tax_paid / 100.0)::float as tax_paid,
-
-        ---------- timestamps
-        ordered_at
-
-    from source
-
-)
-
-select * from renamed
+select
+    id as order_id,
+    user_id as customer_id,
+    order_date,
+    status
+from {{ source('jaffle_shop', 'orders') }}
---
+++ models/staging/stg_payments.sql
@@ -0,0 +1,15 @@
+with payments as (
+    
+    select
+        id as payment_id,
+        orderid as order_id,
+        paymentmethod as payment_method,
+        status,
+       {{cents_to_dollars('amount')}} as amount,
+        created as created_at
+
+    from {{ source('stripe', 'payment') }}
+
+)
+
+select * from payments
